5f9e277d85a0be599fe8f8ef24904b7d08e3c8af
Wandalen <wandalen.me@gmail.com>
2018-07-18 10:01:18 +0300

    publish

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 Mixin providing file storage and save/load methods.
 
+
--- staging/dwtools/amid/mixin/aFileStorageMixin.s
+++ staging/dwtools/amid/mixin/aFileStorageMixin.s
@@ -2,6 +2,32 @@
 
 'use strict';
 
+if( typeof module !== 'undefined' )
+{
+
+  if( typeof _global_ === 'undefined' || !_global_.wBase )
+  {
+    let toolsPath = '../../../dwtools/Base.s';
+    let toolsExternal = 0;
+    try
+    {
+      toolsPath = require.resolve( toolsPath );
+    }
+    catch( err )
+    {
+      toolsExternal = 1;
+      require( 'wTools' );
+    }
+    if( !toolsExternal )
+    require( toolsPath );
+  }
+
+  var _ = _global_.wTools;
+
+  _.include( 'wProto' );
+
+}
+
 //
 
 var _global = _global_;
